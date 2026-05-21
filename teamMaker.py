ed5f5cf061497e6117c7f104412202fe1ae6c34b
rj-here <r.rishi.rj@gmail.com>
2026-05-20 21:50:41 -0400

    Progress

--- teamMaker.py
+++ teamMaker.py
@@ -1,9 +1,17 @@
 #This is going to be a simple team maker program.
-def takeNames(peopleCount):
-    #This function takes in names
-    listOfNames = [] #List of names.
-    while len(listOfNames < peopleCount):
-        name = input("Please put in a name.")
-        listOfNames.__add__(name)
+def takeName():
+    name = input("What is your name? ") #Takes the name
+    print(name)
 
-takeNames(5)
+def playerCount():
+    count = input("How many players are involved in total? ") #Takes number of players
+    print(count)
+
+def nameSets():
+    count = playerCount()
+    names = []
+    i = 0 #initalize
+    while i < count:
+        names.__add__(takeName())
+
+nameSets()
