34d1b87bfe0058396a41e7f38dcd8f3728731362
rj-here <r.rishi.rj@gmail.com>
2026-05-21 17:08:06 -0400

    Changes, progress

--- teamMaker.py
+++ teamMaker.py
@@ -1,17 +1,25 @@
 #This is going to be a simple team maker program.
-def takeName():
-    name = input("What is your name? ") #Takes the name
-    print(name)
+def takeName(count):
+    names = set()
+    for i in range(0, count): 
+        #Don't be rusty | https://wiki.python.org/python/ForLoop.html
+        name = input("What is your name? ") #Takes the name
+        names.add(name)
+    return names
 
 def playerCount():
-    count = input("How many players are involved in total? ") #Takes number of players
-    print(count)
+    count = int(input("How many players are involved in total? ")) #Takes number of players
+    print(f"${count} players involved.")
+    return count
 
 def nameSets():
     count = playerCount()
-    names = []
-    i = 0 #initalize
-    while i < count:
-        names.__add__(takeName())
+    print(count)
+    names = takeName(count)
+
+def teamCount():
+    team = int(input("How many teams do you want? ")) #Number of teams
+    return team
 
 nameSets()
+teamCount()
